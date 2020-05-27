133d26f9b2e234ec67d97fb21b571cf34b8d0c39
zengge77 <936345336@qq.com>
2020-05-28 01:08:34 +0800

    新增：NonBreakingSpaceTextComponent UI工具类
    可将Text中的换行空格替换为不换行空格

---
+++ Assets/Script/UI/Tool/NonBreakingSpaceTextComponent.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+[RequireComponent(typeof(Text))]
+public class NonBreakingSpaceTextComponent : MonoBehaviour
+{
+    /* 我们平时所使用的空格（即键盘Sapce键输出的空格），Unicode编码为/u0020，是换行空格(Breaking Space)，
+     * 本意是用在英文的自动换行，但在中文里反而是个麻烦。
+     * 我们使用 不换行空格(Non-breaking space)，Unicode编码为/u00A0 ，来替换换行空格
+     * 将该脚本放在需要输出文字的UI下即可
+    */
+
+    private const string no_breaking_space = "\u00A0";
+
+    private Text text;
+
+    void Awake()
+    {
+        text = this.GetComponent<Text>();
+        text.RegisterDirtyVerticesCallback(OnTextChange);
+    }
+
+    public void OnTextChange()
+    {
+        if (text.text.Contains(" "))
+        {
+            text.text = text.text.Replace(" ", no_breaking_space);
+        }
+    }
+
+}
