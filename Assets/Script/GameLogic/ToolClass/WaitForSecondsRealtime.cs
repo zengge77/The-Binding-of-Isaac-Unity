c1dca5de238c447f79c403aca23847710e7fcb19
zengge77 <936345336@qq.com>
2020-09-29 15:38:50 +0800

    新增：WaitForSecondsRealtime类
    用于使用协程时获取一个不受Time.timeScale影响的时间。
    例子：yield return new WaitForSecondsRealtime(ActiveSceond);

---
+++ Assets/Script/GameLogic/ToolClass/WaitForSecondsRealtime.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 不受Time.timeScale影响的等待时间
+/// </summary>
+public class WaitForSecondsRealtime : CustomYieldInstruction
+{
+    private float waitTime;
+
+    public override bool keepWaiting
+    {
+        get
+        {
+            return Time.realtimeSinceStartup < waitTime;
+        }
+    }
+
+    public WaitForSecondsRealtime(float time)
+    {
+        waitTime = Time.realtimeSinceStartup + time;
+    }
+}
