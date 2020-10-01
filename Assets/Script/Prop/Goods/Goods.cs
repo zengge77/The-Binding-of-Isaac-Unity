f876987df613a8b9e231eca37c21d15e4621f20d
zengge77 <936345336@qq.com>
2020-10-01 13:54:56 +0800

    修复：红心类商品购买后消失
    原因：商品类由商品本身和道具组成，且道具关闭了SpriteRenderer。商品触发购买后，道具开启碰撞体，但是红心道具只在非满血下才会被玩家吃到，于是被玩家撞开，且因为红心是不可见的，造成了购买后消失的假象。
    修复：商品下的道具会在被购买后开启SpriteRenderer。

--- Assets/Script/Prop/Goods/Goods.cs
+++ Assets/Script/Prop/Goods/Goods.cs
@@ -8,21 +8,21 @@ public class Goods : Prop
     public enum Goodstype { Item, Pickup }
     public Goodstype goodstype;
 
-    private GameObject prop;
     private GameObject newProp;
     private int price;
 
     private void Start()
     {
+        GameObject prototypeProp = null;
         //获取道具
         switch (goodstype)
         {
             case Goodstype.Item:
-                prop = pools.GetProp(Pools.PropPoolType.Shop).gameObject;
+                prototypeProp = pools.GetProp(Pools.PropPoolType.Shop).gameObject;
                 price = 15;
                 break;
             case Goodstype.Pickup:
-                prop = pools.GetPickupGoods();
+                prototypeProp = pools.GetPickupGoods();
                 price = 3;
                 break;
             default:
@@ -31,15 +31,14 @@ public class Goods : Prop
 
         //生成道具
         Transform propContainer = level.currentRoom.PropContainer;
-        newProp = Instantiate(prop, propContainer);
+        newProp = Instantiate(prototypeProp, propContainer);
         newProp.transform.position = transform.position;
         newProp.GetComponent<Collider2D>().enabled = false;
         newProp.GetComponent<SpriteRenderer>().enabled = false;
 
         //设置价格，UI
-        GetComponent<SpriteRenderer>().sprite = prop.GetComponent<SpriteRenderer>().sprite;
+        GetComponent<SpriteRenderer>().sprite = newProp.GetComponent<SpriteRenderer>().sprite;
         GetComponentInChildren<TextMesh>().text = price.ToString();
-
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -59,7 +58,9 @@ public class Goods : Prop
     protected override void Effect()
     {
         player.CoinNum -= price;
+        UI.attributes.UpDateAttributes();
         newProp.GetComponent<Collider2D>().enabled = true;
+        newProp.GetComponent<SpriteRenderer>().enabled = true;
     }
 
     protected override void After()
